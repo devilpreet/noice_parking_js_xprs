87e4607d95d4fc53344e44c51391965104f832c4
s.bhatti <s.bhatti@jampas.net>
2022-05-31 01:19:07 +0530

    Adding morgan express logger

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const CONFIG = require('./src/config')
 const express = require('express')
+const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const app = express()
 
 /* Middlewares */
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(bodyParser.json())
+app.use(morgan('combined'))
 
 app.listen(CONFIG.PORT, () => {
   console.log("Server is running on", CONFIG.PORT)
